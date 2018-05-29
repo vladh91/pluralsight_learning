adf4637e61b9810c1917abed7f8150b35f251db1
Vlad Harutyunyan <harutyunyan.vlad@gmail.com>
2018-05-29 22:46:20 +0400

    Initial commit

---
+++ loops.py
@@ -0,0 +1,8 @@
+student_names=["James", "Katarina", "Jessica", "Mark", "Bert", "Frank Grimas",
+               "Max Power"]
+
+for name in student_names:
+    if name == "Bort":
+        continue
+        print("Found him! " + name)
+    print("Currently testing " + name)
